94c122f722084cf989b9d3955a9d6c780bbdd825
ririErliza <ryantiadam@yahoo.co.uk>
2021-12-03 15:44:05 +0100

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,23 @@
+/* EXERCISE 1
+ Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
+*/
+
+/*const area = function(l1,l2) {
+    return l1*l2;
+}
+*/
+
+function square (length,width) {
+    return length*width;
+}
+square(20,100);
+
+/* EXERCISE 2
+ Write a function called "crazySum" which receives two integers as parameters.
+ It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
+*/
+
+function crazySum (ig1, ig2){
+    
+}
+
